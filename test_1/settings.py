762ce99601b3ea9188e50f62d915d9b1ae16ce99
Nearlz <patriciobarra.99@gmail.com>
2021-09-13 00:46:59 -0300

    Update settings.py

--- test_1/settings.py
+++ test_1/settings.py
@@ -14,12 +14,16 @@ import cloudinary_storage
 import environ
 from pathlib import Path
 
-env = environ.Env()
-environ.Env.read_env()
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(BASE_DIR,'.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
